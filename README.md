0e4478dc1c9203f6dab129677add85b096822e7a
Edwin Yip <edwin.yip@innovationgear.com>
2023-04-18 17:08:20 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # simple-sempare-template-engine-mormot1-demo
+
+
+A very simple console program that demonstrates [Sempare Template Engine for Delphi](https://github.com/sempare/sempare-delphi-template-engine) + [mORMot (v1)](https://github.com/synopse/mORMot)
+
+
+Compile and run the program, a mORMot web server will be started and your browser will open http://localhost/www/home which is a deadly simple html page rendered by Sempare Template Engine.
+
+Compiler: Delphi XE4
---
+++ WebServer.dpr
@@ -0,0 +1,65 @@
+program WebServer;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils, SynCommons, mORMot, mORMotHttpServer, Sempare.Template, Winapi.Windows, Winapi.ShellAPI;
+
+type
+  // Note: TSQLRestServerFullMemory has no database support
+  TMyRestServer = class(TSQLRestServerFullMemory)
+  published
+    procedure Home(Ctxt: TSQLRestServerURIContext);
+  end;
+
+  TPageData = record
+    UserAgent: string;
+    TmplEngineName: string;
+    WebServrName: string;
+  end;
+
+procedure TMyRestServer.Home(Ctxt: TSQLRestServerURIContext);
+var
+  htmlTmpl, htmlResult: string;
+  pageData: TPageData;
+begin
+  // Populate the page data
+  pageData.UserAgent := Ctxt.UserAgent;
+  pageData.TmplEngineName := 'Sempare Template Engine for Delphi';
+  pageData.WebServrName := 'mORMot v1';
+
+  // Load html template from file
+  htmlTmpl := StringFromFile(ExtractFilePath(ParamStr(0)) + 'Home-template.html');
+
+  // Render it
+  htmlResult := Template.Eval(htmlTmpl, pageData);
+
+  // Return the html page to the web browser
+  Ctxt.Returns(htmlResult, HTTP_SUCCESS, HTML_CONTENT_TYPE_HEADER);
+end;
+
+const
+  cWebServerAddr = 'http://localhost/www/home';
+var
+  dbModel: TSQLModel;
+  restSvr: TMyRestServer;
+  httpSvr: TSQLHttpServer;
+begin
+  dbModel := TSQLModel.Create([],'www');
+  restSvr := TMyRestServer.Create(dbModel);
+  dbModel.Owner := restSvr;
+
+  httpSvr := TSQLHttpServer.Create('80', [restSvr], '+', useHttpSocket, 8);
+  httpSvr.AccessControlAllowOrigin := '*';
+
+  WriteLn('');
+  WriteLn('Demonstrating mORMot 1''s method-based service serving html page rendered by Sempare Template Engine.');
+  WriteLn('');
+  WriteLn('Please visit ' + cWebServerAddr);
+  write('Press [Enter] to close the web server.');
+  ShellExecute(0, 'open', PChar(cWebServerAddr), nil, nil, SW_SHOWNORMAL);
+  ReadLn;
+
+  httpSvr.Free;
+  restSvr.Free;
+end.
